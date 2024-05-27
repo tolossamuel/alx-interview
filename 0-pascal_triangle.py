a31fc5565e8688efacde7b7ad2318872406d4d75
tolossamuel <tolossamuel1@gmail.com>
2024-05-27 13:02:54 +0300

    updat

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+0-main
+"""
+def pascal_triangle(n):
+    arr = [[1]]
+    for i in range(n-1):
+        temp = [1]
+        for x in range(1,len(arr[-1])):
+            temp.append(arr[-1][x-1]+arr[-1][x])
+        temp.append(1)
+        arr.append(temp.copy())
+    for x in arr:
+        print(x)
+
+pascal_triangle(5)
+            
+        
